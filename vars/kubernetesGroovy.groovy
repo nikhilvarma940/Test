9a8ca27dea2c59839bf15a9e58ee9739f8645784
NIKHIL VARMA M <63706239+nikhilvarma940@users.noreply.github.com>
2023-03-14 07:46:32 +0530

    Update kubernetesGroovy.groovy

--- vars/kubernetesGroovy.groovy
+++ vars/kubernetesGroovy.groovy
@@ -1,1 +1,53 @@
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
 
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+		registry = "$registryin" 	
+		dockerTag = "${docTag}_$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+	}
+		
+	agent { label 'k8s_slave' }
+	
+
+	stages {
+		stage("GIT SCM"){
+			steps {
+				 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
+			}
+		}	
+					
+		stage('BUILD IMAGE') { 
+			 steps { 
+				 script { 
+					 dockerImage = docker.build("$registry:$dockerTag") 
+				 }
+			} 
+		}
+					
+		stage('PUSH HUB') { 
+			 steps { 
+				 script { 
+					 docker.withRegistry( '', "$registryCredential" ) { 
+						 dockerImage.push() 
+					}
+				}		
+			} 
+		}
+					
+		stage('DEPLOY') {
+			steps {
+				script { 
+					      sh 'kubectl set image deployment
+					}
+				} 
+			}
+		}
+	}
+			  
+}
+
+}
